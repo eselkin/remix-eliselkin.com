89e957ab6d39874e633f4352c91aef4acac37d79
Eli Selkin <eli@eliselkin.com>
2024-08-11 18:37:13 -0700

    Resume title

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,16 @@
+import { MetaFunction } from "@remix-run/node";
 import ResumeComponent from "~/components/Resume";
 import PageWithDrawer from "~/layouts/PageWithDrawer";
 
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Eli Selkin | Resume" },
+    {
+      property: "og:title",
+      content: "Eli Selkin | Resume",
+    },
+  ];
+};
 export default function Resume() {
   return <PageWithDrawer mainChildren={<ResumeComponent />} />;
 }
